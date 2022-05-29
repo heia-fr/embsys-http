508fa98f76be6cc1fcb52f8375814a52cd11cbb5
Jacques Supcik <jacques.supcik@hefr.ch>
2022-05-29 22:05:40 +0200

    wip

---
+++ src/http.c
@@ -0,0 +1,10 @@
+#include "http_parsed_url.h"
+#include "http_parser.h"
+#include "http_request.h"
+#include "http_request_base.h"
+#include "http_request_builder.h"
+#include "http_request_parser.h"
+#include "http_response.h"
+#include "https_request.h"
+
+// Empty stubs
